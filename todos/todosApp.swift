906f3068479b71ae6c8da8813ceda02fe21b704d
Marcin Jędrzejak <53284922+maartinj@users.noreply.github.com>
2025-05-23 13:48:16 +0200

    Initial Commit

---
+++ todos/todosApp.swift
@@ -0,0 +1,17 @@
+//
+//  todosApp.swift
+//  todos
+//
+//  Created by Marcin Jędrzejak on 23/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct todosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
