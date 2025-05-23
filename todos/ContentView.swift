cc04a0398f570ac77a1ebdddf58c99cce495d472
Marcin Jędrzejak <53284922+maartinj@users.noreply.github.com>
2025-05-23 14:19:07 +0200

    feat: A Tour of the Composable Architecture: Part 1

--- todos/ContentView.swift
+++ todos/ContentView.swift
@@ -5,20 +5,96 @@
 //  Created by Marcin Jędrzejak on 23/05/2025.
 //
 
+import ComposableArchitecture
 import SwiftUI
 
+struct Todo: Equatable, Identifiable {
+    var description = ""
+    let id: UUID
+    var isComplete = false
+}
+
+struct AppState: Equatable {
+    var todos: [Todo]
+}
+
+enum AppAction {
+    case todoCheckboxTapped(index: Int)
+    case todoTextFieldChanged(index: Int, text: String)
+}
+
+struct AppEnvironment {
+
+}
+
+let appReducer = Reducer<AppState, AppAction, AppEnvironment> { state, action, environment in
+    switch action {
+    case .todoCheckboxTapped(index: let index):
+        state.todos[index].isComplete.toggle()
+        return .none
+
+    case .todoTextFieldChanged(index: let index, text: let text):
+        state.todos[index].description = text
+        return .none
+    }
+}
+.debug()
+
 struct ContentView: View {
+    let store: Store<AppState, AppAction>
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            WithViewStore(self.store) { viewStore in
+                List {
+                    ForEach(Array(viewStore.todos.enumerated()), id: \.element.id) { index, todo in
+                        HStack {
+                            Button(action: { viewStore.send(.todoCheckboxTapped(index: index))}) {
+                                Image(systemName: todo.isComplete ? "checkmark.square" : "square")
+                            }
+                            .buttonStyle(PlainButtonStyle())
+
+                            TextField(
+                                "Untitled todo",
+                                text: viewStore.binding(
+                                    get: { $0.todos[index].description },
+                                    send: { .todoTextFieldChanged(index: index, text: $0)}
+                                )
+                            )
+                        }
+                        .foregroundStyle(todo.isComplete ? .gray : nil)
+                    }
+                }
+                .navigationTitle("Todos")
+            }
         }
-        .padding()
     }
 }
 
 #Preview {
-    ContentView()
+    ContentView(
+        store: Store(
+            initialState: AppState(
+                todos: [
+                    Todo(
+                        description: "Milk",
+                        id: UUID(),
+                        isComplete: false
+                    ),
+                    Todo(
+                        description: "Eggs",
+                        id: UUID(),
+                        isComplete: false
+                    ),
+                    Todo(
+                        description: "Hand Soap",
+                        id: UUID(),
+                        isComplete: true
+                    )
+                ]
+            ),
+            reducer: appReducer,
+            environment: AppEnvironment()
+        )
+    )
 }
--- todos/todosApp.swift
+++ todos/todosApp.swift
@@ -5,13 +5,20 @@
 //  Created by Marcin Jędrzejak on 23/05/2025.
 //
 
+import ComposableArchitecture
 import SwiftUI
 
 @main
 struct todosApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(
+                store: Store(
+                    initialState: AppState(),
+                    reducer: appReducer,
+                    environment: AppEnvironment()
+                )
+            )
         }
     }
 }
